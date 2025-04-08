26925ef408c8cadf8cda9ba294e5b2763549f0c2
ragOP <najmiraghib@gmail.com>
2025-04-08 17:45:07 +0530

    dsd

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Chatbot from './Chatbot';
 import ChatbotTwo from './Chatbottwo';
 import ChatbotThree from './ChatbotThree';
 import ChatbotFour from './ChatbotFour';
+import Chatbotdq from './Chatbotdq';
 import Home from './Home';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/engsf1" element={<Chatbot />} />
+        <Route path="/engsfdq" element={<Chatbotdq />} />
         <Route path="/engsf2200" element={<ChatbotTwo />} />
         <Route path="/engsf1dup" element={<ChatbotThree />} />
         <Route path="/engsf2200dup" element={<ChatbotFour />} />
